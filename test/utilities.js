2c1841ec87f31e8373c4c3674db71b1c39d410f0
Adnan Begovic <begovic.adnan@ymail.com>
2018-05-09 17:35:37 +0200

    add dockerfile and change utilities

--- test/utilities.js
+++ test/utilities.js
@@ -1,8 +1,7 @@
-const should = require('chai').should(),
-      expect = require('chai').expect,
-      supertest = require('supertest'),
-      api = supertest('http://localhost:8000'),
-      jwt_decode = require('jwt-decode')
+const should = require("chai").should(),
+  expect = require("chai").expect,
+  supertest = require("supertest"),
+  api = supertest(process.env.URL || "http://localhost:8000"),
+  jwt_decode = require("jwt-decode");
 
-
-module.exports = { should, expect, api, jwt_decode }
+module.exports = { should, expect, api, jwt_decode };
